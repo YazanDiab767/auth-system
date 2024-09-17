0b6315b421d8349cb97906ff3b14cf3ef429f8ca
YazanDiab767 <yazandiab676@gmail.com>
2024-09-17 20:57:55 +0300

    finish all email auth, register and rest password

--- users/urls.py
+++ users/urls.py
@@ -3,14 +3,25 @@ from tkinter.font import names
 from django.urls import path, include
 from . import views
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.login_view, name="login"),
     path('register/', views.register_view, name='register'),
     path('home/', views.home_view, name='home'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('activate/<str:email>/', views.activate, name='activate'),
+    path('activate/<path:uidb64>/<path:token>/', views.activate, name='activate'),
 
-    path('test/', views.send_test_email, name='test_email'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,30 +1,46 @@
+import time
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.shortcuts import get_object_or_404
-from django.utils.translation import gettext as _
-
 from users.forms import UserRegistrationForm
-
-from django.utils import timezone
 from django.urls import reverse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-
 from users.models import CustomUser
+from django.contrib.auth.tokens import default_token_generator
+from django.core.signing import Signer, BadSignature
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.urls import reverse
+from django.template.loader import render_to_string
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib.auth.views import PasswordResetView
+from django.urls import reverse_lazy
+
+token_generator = PasswordResetTokenGenerator()
 
 
 def login_view(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
+        remember_me = request.POST.get('remember_me')
+
+        if remember_me:
+            request.session.set_expiry(1209600)  # 2 weeks in seconds
+        else:
+            request.session.set_expiry(0)
 
         user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, 'Login successful! Welcome ' + user.username + '!')
             return redirect('home')
         else:
             return render(request, 'login_page.html', {'error': 'Invalid email or password', 'email': email})
@@ -40,8 +56,12 @@ def register_view(request):
             user.set_password(form.cleaned_data['password'])
             user.save()
 
+            # Create a unique token for activation
+            uid = urlsafe_base64_encode(force_bytes(user.email))
+            token = token_generator.make_token(user)
+
             # Send activation email
-            activation_link = request.build_absolute_uri(reverse('activate', args=[user.email]) )
+            activation_link = request.build_absolute_uri(reverse('activate', args=[uid,token]) )
 
             subject = 'Activate Your Account'
             html_message = render_to_string('activation_email.html', {'activation_link': activation_link})
@@ -60,12 +80,22 @@ def register_view(request):
 def home_view(request):
     return render(request, "home_page.html")
 
-def activate(request, email):
-    user = get_object_or_404(CustomUser, email=email)  # Use email to get the user
-    user.is_active = True  # Activate the user account
-    user.save()
-    messages.success(request, 'Your account has been activated. You can now log in.')
-    return redirect(request, 'login')
+def activate(request, uidb64, token):
+
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = get_user_model().objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
+        user = None
+
+    if user is not None and token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Your account has been activated. You can now log in.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Activation link is invalid or has expired.')
+        return redirect('login')
 
 
 def send_test_email(request):
